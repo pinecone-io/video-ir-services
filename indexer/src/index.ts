9c5f8526a38c9bcf95a257039863ef0b8e0ff5e3
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-10-18 21:37:47 -0700

    missing bit

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -11,6 +11,7 @@ import { initIndex } from "./utils/pinecone";
 import { KafkaMessage } from "kafkajs";
 import { createKafkaConsumer } from "./utils/kafka-consumer";
 import { indexImages } from "./indexImages";
+import EventEmitter from "events";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -21,24 +22,39 @@ const app: Express = express();
 await initIndex();
 
 
-let filesList: string[] = [];
-let processedFiles: string[] = [];
+let isProcessing = false;
+const messageQueue: KafkaMessage[] = [];
+
+const messageEvent = new EventEmitter();
+
 const consumer = await createKafkaConsumer("unprocessed-files-3", async (message: KafkaMessage) => {
-
-
-  const file = message.value?.toString();
-  if (file) {
-    filesList.push(file);
-  }
-  if (filesList.length >= 5 || true) {
-    const time = new Date().toISOString();
-    await indexImages({ filesList });
-    // console.log("processing", filesList, time)
-    processedFiles.push(...filesList);
-    filesList = [];
-
-  }
+  messageQueue.push(message);
+  messageEvent.emit('newMessage');
 });
+
+
+const processMessages = async () => {
+  if (isProcessing) {
+    console.log("still processing");
+    return;
+  }
+
+  while (messageQueue.length > 0) {
+    const message = messageQueue.shift();
+    const file = message?.value?.toString();
+
+    if (!isProcessing) {
+      isProcessing = true;
+      await indexImages({ filesList: [file!] });
+      isProcessing = false;
+    } else {
+      process.stdout.write(".");
+    }
+  }
+  if (messageQueue.length === 0) {
+    console.log("done processing");
+  }
+};
 
 messageEvent.on('newMessage', processMessages);
 
