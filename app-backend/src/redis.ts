8e7e35c8e50851f1507d001345d594b77a5753ef
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-10-18 21:23:05 -0700

    latest

--- app-backend/src/redis.ts
+++ app-backend/src/redis.ts
@@ -1,9 +1,18 @@
 import { createClient } from "redis";
 
-const redis = createClient();
+
+const redis = createClient({
+    // url: `process.env.REDIS_HOST}:${process.env.REDIS_PORT || '6379'}`
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT || '6379')
+    }
+});
 
 redis.on("error", (err) => console.log("Redis Client Error", err));
 
 await redis.connect();
 
 export default redis;
+
+
