89ca604f6f734dbf5200eb6e9c074a2a0fb8123d
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-10-23 17:24:21 -0700

    light cleanup

--- indexer/src/utils/kafka-consumer.ts
+++ indexer/src/utils/kafka-consumer.ts
@@ -1,6 +1,4 @@
 import { Kafka, Consumer, KafkaMessage, logLevel } from "kafkajs";
-import retry from 'async-retry';
-import { POD_NAME } from "./environment";
 
 
 const KAFKA_BROKER = "kafka-dev"
@@ -12,25 +10,19 @@ async function createKafkaConsumer(messageHandler: (message: KafkaMessage) => vo
         logLevel: logLevel.INFO
 
     });
-    const topics = ["topic-0"]
-
-    const consumerGroupId = `group-1`; // Assuming POD_NAME is unique for each consumer
+    const topic = "topic-0";
+    const consumerGroupId = "group-1"; // This has to be the same for all consumers
     const consumer = kafka.consumer({ groupId: consumerGroupId });
 
     try {
         await consumer.connect();
-        await consumer.subscribe({ topic: topics[0]!, fromBeginning: false });
+        await consumer.subscribe({ topic, fromBeginning: false });
         await consumer.run({
-            // eachMessage: async ({ topic, partition, message }) => {
-            //     console.log("Received message", message?.value?.toString())
-            //     messageHandler(message);
-            // },
-
             eachBatch: async ({ batch, resolveOffset, heartbeat, isRunning, isStale }) => {
                 for (let message of batch.messages) {
                     console.log(message?.value?.toString());
 
-                    if (!isRunning() || isStale()) {
+                    if (!isRunning() || isStale()) { // This ensures we're not handling messages that were already handled 
                         break;
                     }
 
