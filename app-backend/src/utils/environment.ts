04ecf6e0266250b9eb5b1fb96513ebecf1baae21
Dejan Miletic <dejan.miletic@smartcat.io>
2023-10-30 16:34:49 +0100

    Added envirometn into k8

--- app-backend/src/utils/environment.ts
+++ app-backend/src/utils/environment.ts
@@ -1,5 +1,5 @@
 export const getEnv = (key: string): string => {
-  const value = import.meta.env[key];
+  const value = import.meta.env[key] || process.env[key];
   if (!value) {
     throw new Error(`${key} environment variable not set`);
   }
