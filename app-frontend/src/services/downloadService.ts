c4f7c51200c81f55dc676e874a7242f2306f3bb0
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-10-31 16:43:06 -0700

    services are all finally working

--- app-frontend/src/services/downloadService.ts
+++ app-frontend/src/services/downloadService.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 
-const DOWNLOAD_API = "http://localhost:3000/api/download";
+const DOWNLOAD_API = "/api/download";
 
 export const download = (target: string, name: string, fps: number): Promise<AxiosResponse<string>> =>
   axios.post(`${DOWNLOAD_API}`, { target, fps, name })
